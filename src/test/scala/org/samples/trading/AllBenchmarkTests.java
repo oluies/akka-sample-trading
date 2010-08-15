53f2486cd7aae2e5ceb1892f5fa7aa76bcec609f
patriknw <patrik.nordwall@gmail.com>
2010-08-15 09:16:01 +0200

    distribution assembly

--- src/test/scala/org/samples/trading/AllBenchmarkTests.java
+++ src/test/scala/org/samples/trading/AllBenchmarkTests.java
@@ -3,12 +3,19 @@ package org.samples.trading;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.samples.trading.actor.ActorPerformanceTest;
+import org.samples.trading.akka.AkkaPerformanceTest;
+import org.samples.trading.akkabang.AkkaBangPerformanceTest;
+import org.samples.trading.akkahawt.AkkaHawtPerformanceTest;
 import org.samples.trading.basic.BasicPerformanceTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { 
   BasicPerformanceTest.class, 
-  ActorPerformanceTest.class
+  ActorPerformanceTest.class,
+  AkkaPerformanceTest.class,
+  AkkaBangPerformanceTest.class,
+  AkkaHawtPerformanceTest.class
+  
 })
 public class AllBenchmarkTests {
 
